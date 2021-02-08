0fafd4e135e352d8516a43a5f0258f29236fb965
tomcrane <tom@nanoplanet.com>
2021-02-08 15:54:46 +0000

    search 1 in Presentation 3 manifests

--- 3/script/common.js
+++ 3/script/common.js
@@ -6,7 +6,7 @@ var wcOrgProd = "https://iiif.wellcomecollection.org";
 
 $(function() {
     $('#imfeelinglucky').on('click', ImFeelingLucky);
-    $('.typeahead').typeahead({
+    $('#schBox').typeahead({
         minLength: 4,
         highlight: true
     },
@@ -17,13 +17,13 @@ $(function() {
         limit: 50,
         display: formatFlatManifestation
     });
-    $('.typeahead').bind('typeahead:select', function(ev, suggestion) {
+    $('#schBox').bind('typeahead:select', function(ev, suggestion) {
         loadSuggestion(suggestion);
     });
-    $('.typeahead').bind('typeahead:asyncrequest', function(ev) {
+    $('#schBox').bind('typeahead:asyncrequest', function(ev) {
         $('#typeaheadWait').show();
     });
-    $('.typeahead').bind('typeahead:render', function(ev) {
+    $('#schBox').bind('typeahead:render', function(ev) {
         $('#typeaheadWait').hide();
     });
 });
@@ -100,3 +100,127 @@ function langMap(langMap){
     }
     return null;
 }
+
+
+function getSearchService(manifest){            
+    if(manifest.service)
+    {
+        for(si = 0; si<manifest.service.length; si++){
+            var svc = manifest.service[si];
+            if(svc && svc.profile && svc.profile == "http://iiif.io/api/search/1/search"){
+                return svc;
+            }
+        }
+    }
+    return null;
+}
+
+
+function getThumb(canvas, thumbSize){
+    if(!canvas.thumbnail){
+        return null;
+    }
+    var thumb = canvas.thumbnail[0].id;
+    if(canvas.thumbnail[0].service && canvas.thumbnail[0].service[0].sizes){
+        // manifest gives thumb size hints
+        // dumb version exact match and assumes ascending - TODO: https://gist.github.com/tomcrane/093c6281d74b3bc8f59d
+        var particular = getParticularSizeThumb(canvas, thumbSize);
+        if(particular) return particular;
+    }
+    return thumb;
+}
+
+function getParticularSizeThumb(canvas, thumbSize){
+    var sizes = canvas.thumbnail[0].service[0].sizes;
+    for(var i=sizes.length - 1; i>=0; i--){
+        if((sizes[i].width == thumbSize || sizes[i].height == thumbSize) && sizes[i].width <= thumbSize && sizes[i].height <= thumbSize){
+            // this is still an ImageService2
+            return canvas.thumbnail[0].service[0]["@id"] + "/full/" + sizes[i].width + "," + sizes[i].height + "/0/default.jpg";
+        }
+    }
+    return null;
+}
+
+
+function getThumbHtml(canvas, thumbSize, elementId){
+    let thumbHtml = '<div class="tc">' + (langMap(canvas.label) || '') + '<br/>';
+    let thumb = getThumb(canvas, thumbSize);
+    if(!thumb){ 
+        thumbHtml += '<div class="thumb-no-access">Image not available</div></div>';
+    } else {
+        // This is a temporary hack to get sizes until sorty, getThumbnail etc are unified with this
+        let sizeParam = /full\/([^\/]*)\/(.*)/g.exec(thumb);
+        let dimensions = "";
+        if(sizeParam && sizeParam[1] && sizeParam[1].indexOf(",") > 0) {
+            wh = sizeParam[1].split(",");
+            dimensions = "width='" + wh[0] + "' height='" + wh[1] + "'";
+        }
+        let id = "";
+        if(elementId){
+            id = "id='" + elementId + "'";
+        }
+        thumbHtml += '<img ' + id + ' class="thumb" title="' + langMap(canvas.label) + '" data-uri="' + canvas.id + '" data-src="' + thumb + '" ' + dimensions + '/></div>';
+    }
+    return thumbHtml;
+}
+
+
+
+/**
+ * jQuery Unveil
+ * A very lightweight jQuery plugin to lazy load images
+ * http://luis-almeida.github.com/unveil
+ *
+ * Licensed under the MIT license.
+ * Copyright 2013 Lu�s Almeida
+ * https://github.com/luis-almeida
+ */
+
+; (function ($) {
+
+    $.fn.unveil = function (threshold, callback) {
+
+        var $v = $(".viewer"), $w = $(window),
+            th = threshold || 0,
+            retina = window.devicePixelRatio > 1,
+            attrib = retina ? "data-src-retina" : "data-src",
+            images = this,
+            loaded;
+
+        this.one("unveil", function () {
+            var source = this.getAttribute(attrib);
+            source = source || this.getAttribute("data-src");
+            if (source) {
+                console.log("setting src " + source);
+                this.setAttribute("src", source);
+                if (typeof callback === "function") callback.call(this);
+            }
+        });
+
+        function unveil() {
+            var inview = images.filter(function () {
+                var $e = $(this);
+                if ($e.is(":hidden")) return;
+
+                var wt = $w.scrollTop(),
+                    wb = wt + $w.height(),
+                    et = $e.offset().top,
+                    eb = et + $e.height();
+
+                return eb >= wt - th && et <= wb + th;
+            });
+
+            loaded = inview.trigger("unveil");
+            images = images.not(loaded);
+        }
+
+        $w.on("scroll.unveil resize.unveil lookup.unveil", unveil);
+        $v.on("scroll.unveil resize.unveil lookup.unveil", unveil);
+
+        unveil();
+
+        return this;
+
+    };
+
+})(window.jQuery);
--- 3/script/thumbs.js
+++ 3/script/thumbs.js
@@ -17,7 +17,7 @@ pop += "            <\/div>";
 pop += "            <div class=\"modal-body\">            ";
 pop += "                <div id=\"imgContainer\">";
 pop += "                     <img id=\"bigImage\" class=\"img-responsive osd\" \/>";
-pop += "                     <div id=\"lineHighlight\"></div>";
+pop += "                     <div id=\"lineHighlight\" class=\"lineHighlight\"></div>";
 pop += "                </div>";
 pop += "                <div id=\"viewer\" class=\"osd-viewer\"></div>";
 pop += "                <div class=\"auth-ops\" id=\"authOps\">";
@@ -167,8 +167,15 @@ function load(manifest){
     if(manifest.id.indexOf("wellcome") != -1 || manifest.id.indexOf("localhost") != -1){
         var wcManifestationId = manifest.id.split("/")[4];
         if(wcManifestationId){
-            var uvPage = "http://universalviewer.io/examples/?manifest=" + manifest.id;
-            $('#annoDump').after(" | <a href='" + manifest.id + "'>manifest</a> | <a href='" + uvPage + "'>UV</a> | <a href='" + manifest.homepage[0].id + "'>work page</a> ");
+            let manifestLink = "<a href='" + manifest.id + "'>manifest</a>";
+            let uvLink = "<a href='http://universalviewer.io/examples/?manifest=" + manifest.id + "'>UV</a>";
+            var itemPageLink = "<a href='" + manifest.homepage[0].id + "'>work page</a>";
+            $('#annoDump').after(" | " + manifestLink + " | " + uvLink + " | " + itemPageLink);
+            if(getSearchService(manifest))
+            {
+                var searchLink = "<a href='search.html?manifest=" + manifest.id + "'>search</a>";;
+                $('#annoDump').after(" | " + searchLink);
+            }
         }
     }
     canvasList = manifest.items;
@@ -179,25 +186,11 @@ function load(manifest){
 }
 
 function drawThumbs(){
-    var thumbs = $('#thumbs');
+    let thumbs = $('#thumbs');
     thumbs.empty();
-    for(var i=0; i<canvasList.length; i++){
-        var canvas = canvasList[i];
-        var thumbHtml = '<div class="tc">' + (langMap(canvas.label) || '') + '<br/>';
-        var thumb = getThumb(canvas);
-        if(!thumb){ 
-            thumbHtml += '<div class="thumb-no-access">Image not available</div></div>';
-        } else {
-            // This is a temporary hack to get sizes until sorty, getThumbnail etc are unified with this
-            var sizeParam = /full\/([^\/]*)\/(.*)/g.exec(thumb);
-            var dimensions = "";
-            if(sizeParam && sizeParam[1] && sizeParam[1].indexOf(",") > 0) {
-                wh = sizeParam[1].split(",");
-                dimensions = "width='" + wh[0] + "' height='" + wh[1] + "'";
-            }
-            thumbHtml += '<img class="thumb" title="' + langMap(canvas.label) + '" data-uri="' + canvas.id + '" data-src="' + thumb + '" ' + dimensions + '/></div>';
-        }
-        thumbs.append(thumbHtml);
+    let thumbSize = localStorage.getItem('thumbSize');
+    for(let i=0; i<canvasList.length; i++){
+        thumbs.append(getThumbHtml(canvasList[i], thumbSize));
     } 
     $('img.thumb').click(function(){
         selectForModal($(this).attr('data-uri'), $(this));
@@ -372,30 +365,6 @@ function getImageService(canvas){
     return imgService['@id'];
 }
 
-function getThumb(canvas){
-    if(!canvas.thumbnail){
-        return null;
-    }
-    var thumb = canvas.thumbnail[0].id;
-    if(canvas.thumbnail[0].service && canvas.thumbnail[0].service[0].sizes){
-        // manifest gives thumb size hints
-        // dumb version exact match and assumes ascending - TODO: https://gist.github.com/tomcrane/093c6281d74b3bc8f59d
-        var particular = getParticularSizeThumb(canvas, localStorage.getItem('thumbSize'));
-        if(particular) return particular;
-    }
-    return thumb;
-}
-
-function getParticularSizeThumb(canvas, thumbSize){
-    var sizes = canvas.thumbnail[0].service[0].sizes;
-    for(var i=sizes.length - 1; i>=0; i--){
-        if((sizes[i].width == thumbSize || sizes[i].height == thumbSize) && sizes[i].width <= thumbSize && sizes[i].height <= thumbSize){
-            // this is still an ImageService2
-            return canvas.thumbnail[0].service[0]["@id"] + "/full/" + sizes[i].width + "," + sizes[i].height + "/0/default.jpg";
-        }
-    }
-    return null;
-}
 
 function attemptAuth(imageService){
     imageService += "/info.json";
@@ -519,63 +488,3 @@ function getServices(info) {
     }
     return svcInfo;
 }
-
-
-/**
- * jQuery Unveil
- * A very lightweight jQuery plugin to lazy load images
- * http://luis-almeida.github.com/unveil
- *
- * Licensed under the MIT license.
- * Copyright 2013 Lu�s Almeida
- * https://github.com/luis-almeida
- */
-
-; (function ($) {
-
-    $.fn.unveil = function (threshold, callback) {
-
-        var $v = $(".viewer"), $w = $(window),
-            th = threshold || 0,
-            retina = window.devicePixelRatio > 1,
-            attrib = retina ? "data-src-retina" : "data-src",
-            images = this,
-            loaded;
-
-        this.one("unveil", function () {
-            var source = this.getAttribute(attrib);
-            source = source || this.getAttribute("data-src");
-            if (source) {
-                console.log("setting src " + source);
-                this.setAttribute("src", source);
-                if (typeof callback === "function") callback.call(this);
-            }
-        });
-
-        function unveil() {
-            var inview = images.filter(function () {
-                var $e = $(this);
-                if ($e.is(":hidden")) return;
-
-                var wt = $w.scrollTop(),
-                    wb = wt + $w.height(),
-                    et = $e.offset().top,
-                    eb = et + $e.height();
-
-                return eb >= wt - th && et <= wb + th;
-            });
-
-            loaded = inview.trigger("unveil");
-            images = images.not(loaded);
-        }
-
-        $w.on("scroll.unveil resize.unveil lookup.unveil", unveil);
-        $v.on("scroll.unveil resize.unveil lookup.unveil", unveil);
-
-        unveil();
-
-        return this;
-
-    };
-
-})(window.jQuery);
